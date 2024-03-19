9e019f9c868f29ce9be8fcacfcb52af3f72c0b0c
Aditya Vikram Monga <adityavikrammonga@gmail.com>
2024-03-20 02:14:25 +0530

    updated ami

--- ec2_launcher.py
+++ ec2_launcher.py
@@ -13,7 +13,7 @@ def launch_instance(name='app-tier-instance-0', itype='t2.micro'):
         MinCount=1,
         MaxCount=1,
         KeyName='EC2key',
-        ImageId="ami-0dec28adf912e7a8a",
+        ImageId="ami-0679b66fdb562f324",
         SecurityGroupIds=['sg-0e5641dcf17dcae9a'],
         TagSpecifications=[
             {
